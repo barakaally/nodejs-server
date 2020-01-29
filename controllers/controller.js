3d327fde3aa87133024cd99ddfc8ae20dff28610
barakaally <baraka.ally.co.tz@gmail.com>
2020-01-29 15:26:33 +0300

    rename response to ack(acknowledge) in controller.js

--- controllers/controller.js
+++ controllers/controller.js
@@ -4,49 +4,49 @@ class Controller {
 
     }
 
-    static get(request, response) {
+    static get(request, ack) {
         const object = {
             method: request.method,
             url: request.url,
             description: "Try request with other method[POST,PUT,DELETE,GET] or different url"
         }
-        return Action.ok(object, response);
+        return Action.ok(object, ack);
     }
 
-    static post(request, response) {
+    static post(request, ack) {
         const object = {
             method: request.method,
             url: request.url,
             description: "Try request with other method[POST,PUT,DELETE,GET] or different url"
         }
-        return Action.created(object, response);
+        return Action.created(object, ack);
     }
 
-    static put(request, response) {
+    static put(request, ack) {
         const object = {
             method: request.method,
             url: request.url,
             description: "Try request with other method[POST,PUT,DELETE,GET] or different url"
         }
-        return Action.accepted(object, response);
+        return Action.accepted(object, ack);
     }
 
-    static delete(request, response) {
+    static delete(request, ack) {
         const object = {
             method: request.method,
             url: request.url,
             description: "Try request with other method[POST,PUT,DELETE,GET] or different url"
         }
-        return Action.accepted(object, response);
+        return Action.accepted(object, ack);
     }
 
-    static unsupported(request, response) {
+    static unsupported(request, ack) {
         const object = {
             method: request.method,
             url: request.url,
             description: "Try request with other method[POST,PUT,DELETE,GET] or different url"
         }
-        return Action.unsupport(object, response);
+        return Action.unsupport(object, ack);
     }
 }
 
